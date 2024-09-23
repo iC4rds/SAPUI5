2c270b25d12a731e60aac07eab5c35770acc745f
iC4rds <dstaud2005@gmail.com>
2024-09-23 14:59:28 +0200

    2nd

---
+++ script.js
@@ -0,0 +1,12 @@
+var sName;
+var iAlter;
+sName = prompt("gib deinen Namen ein: ");
+iAlter = 5;
+
+console.log (sName); 
+console.log (iAlter);
+
+var iVorherigesAlter = iAlter - 1;
+var iNaechtesAlter = iAlter + 1;
+
+alert("letztes Jahr warst du " + iVorherigesAlter + " Jahre alt. Nächstes Jahr bist du " + iNaechtesAlter + " Jahre alt");
